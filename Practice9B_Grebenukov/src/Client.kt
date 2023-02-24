f9ab57f3890bd3ae3588170ec4980a8550da9081
Ivan <122042934+IvanGrebenyukov@users.noreply.github.com>
2023-02-24 13:39:09 +0300

    Delete Practice9B_Grebenukov directory

--- Practice9B_Grebenukov/src/Client.kt
+++
@@ -1,7 +0,0 @@
-package payments
-
-open class Client(private val name: String, val address: String, val phone: String) {
-    fun Tostring(): String {
-        return "Клиент $name, Адрес $address, Номер телефона $phone"
-    }
-}
--- Practice9B_Grebenukov/src/Order.kt
+++
@@ -1,7 +0,0 @@
-package payments
-
-class Order(val orderid: Int, val amount: Double) {
-    fun Tostring(): String {
-        return "Заказ № $orderid на сумму $amount руб."
-    }
-}
